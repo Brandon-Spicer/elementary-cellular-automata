2ac58fac6035f7c98bd2b91c9dc3ba3e26a1dcaf
Brandon <brandon.david.spicer@gmail.com>
2020-08-29 08:57:24 -0700

    Updates

---
+++ precache-manifest.8e8912734f42b993461197b10dc0f56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda14de7096afa83e615dc2fecd43c4a",
+    "url": "/testRepo/index.html"
+  },
+  {
+    "revision": "39152bd266fb021db9f9",
+    "url": "/testRepo/static/css/main.660ab05e.chunk.css"
+  },
+  {
+    "revision": "b5dcc8b6b623075efd4c",
+    "url": "/testRepo/static/js/2.7620628f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testRepo/static/js/2.7620628f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39152bd266fb021db9f9",
+    "url": "/testRepo/static/js/main.86c7c29d.chunk.js"
+  },
+  {
+    "revision": "d532de82ac203248a07c",
+    "url": "/testRepo/static/js/runtime-main.d7156231.js"
+  }
+]);
